d3c82224ba2589fb3c7a060d05b28c91df94aa45
gregor-grabner <gregor.grabner@gmail.com>
2018-11-15 08:49:56 +0100

    Add Reporting Add Chart.js

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {BookingPage} from "../booking/booking";
 import {AccountPage} from "../account/account";
 import {CategoryPage} from "../category/category";
+import {ReportingPage} from "../reporting/reporting";
 
 
 
@@ -14,7 +15,7 @@ export class TabsPage {
   tab1Root = BookingPage;
   tab2Root = AccountPage;
   tab3Root = CategoryPage;
-  tab4Root = BookingPage;
+  tab4Root = ReportingPage;
 
   constructor() {
 
